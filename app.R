7601f39045078b9aebc69a35fe5e289aeedae22c
Christopher Belanger <christopher.a.belanger@gmail.com>
2026-01-30 18:38:07 +0000

    trim data massively so that it works as a demo

--- app.R
+++ app.R
@@ -7,31 +7,44 @@ library(DT)
 library(readr)
 library(dplyr)
 
+.data <- rlang::.data
+set.seed(1)
 
 ui <- fluidPage(
-  titlePanel("CLOSM - Data Catalogue"),
-  
+  shiny::titlePanel(
+    shiny::tagList(
+      "CLOSM - Data Catalogue",
+      shiny::em("Beta Demo -- Not All Data Included")
+    )
+  ),
+
   # Create a new Row in the UI for selectInputs
   # Populate the select inputs with placeholder values. We will update them
   #   in the server() function below once the data is loaded
-  
+
   fluidRow(
-    column(4,
-           selectInput("data_holding",
-                       "Data Holding",
-                       choices = "Loading...")
+    column(
+      4,
+      selectInput("data_holding",
+        "Data Holding",
+        choices = "Loading..."
+      )
     ),
-    column(4,
-           selectInput("lib",
-                       "Library",
-                       choices = "Loading...")
+    column(
+      4,
+      selectInput("lib",
+        "Library",
+        choices = "Loading..."
+      )
     ),
-    column(4,
-           selectInput("dataset",
-                       "Dataset",
-                       choices = "Loading...")
+    column(
+      4,
+      selectInput("dataset",
+        "Dataset",
+        choices = "Loading..."
+      )
     ),
-    
+
     # FIXME -- commenting these out for now pending advice from Vincent
     # column(4,
     #        selectInput("lang_var",
@@ -46,7 +59,7 @@ ui <- fluidPage(
     # Create a new row for the table.
     DT::dataTableOutput("table")
   ),
-  shiny::downloadButton('downloadData', 'Download')
+  shiny::downloadButton("downloadData", "Download")
 )
 
 
@@ -54,59 +67,80 @@ ui <- fluidPage(
 
 
 server <- function(input, output, session) {
-  
   # Load the pre-processed data:
-  metadata_raw <- readr::read_csv("data/var-trimmed-2024-03-22.csv"
-                                  #, n_max = 10000 # for debugging
+  metadata_raw <- readr::read_csv(
+    "data/var-trimmed-2026-01-30.csv"
   )
-  
-  
+  warning("Demo -- only loading 25000 entries")
+
+
   # Update the select inputs once the data is loaded
-  shiny::updateSelectInput(inputId = "data_holding",
-                           label = "Data Holding",
-                           choices = c("All",
-                                       unique(as.character(metadata_raw$var_data_holding))))
-  
-  shiny::updateSelectInput(inputId = "lib",
-                           label = "Library",
-                           choices = c("All",
-                                       unique(as.character(metadata_raw$var_lib))))
-  
-  
-  shiny::updateSelectInput(inputId = "dataset",
-                           label = "Dataset",
-                           choices = c("All",
-                                       unique(as.character(metadata_raw$var_ds))))
-  
-  
-  shiny::updateSelectInput(inputId = "lang_var",
-                           label = "Language variable",
-                           choices = c("All",
-                                       unique(as.character(metadata_raw$lang_var_confirmed_VMS))))
-  
-  
-  shiny::updateSelectInput(inputId = "lang_off",
-                           label = "Official Language",
-                           choices = c("All",
-                                       unique(as.character(metadata_raw$lang_var_confirmed_VMS))))
-  
+  shiny::updateSelectInput(
+    inputId = "data_holding",
+    label = "Data Holding",
+    choices = c(
+      "All",
+      unique(as.character(metadata_raw$var_data_holding))
+    )
+  )
+
+  shiny::updateSelectInput(
+    inputId = "lib",
+    label = "Library",
+    choices = c(
+      "All",
+      unique(as.character(metadata_raw$var_lib))
+    )
+  )
+
+
+  shiny::updateSelectInput(
+    inputId = "dataset",
+    label = "Dataset",
+    choices = c(
+      "All",
+      unique(as.character(metadata_raw$var_ds))
+    )
+  )
+
+
+  shiny::updateSelectInput(
+    inputId = "lang_var",
+    label = "Language variable",
+    choices = c(
+      "All",
+      unique(as.character(metadata_raw$lang_var_confirmed_VMS))
+    )
+  )
+
+
+  shiny::updateSelectInput(
+    inputId = "lang_off",
+    label = "Official Language",
+    choices = c(
+      "All",
+      unique(as.character(metadata_raw$lang_var_confirmed_VMS))
+    )
+  )
+
   # create a reactive object to hold the filtered data.
   # this logic used to be inside DT::renderDataTable(), but we move it outside
   # so that the filtered data can be re-used in the downloadHandler()
-  reactive_filtered_data <-   shiny::reactive({
+  reactive_filtered_data <- shiny::reactive({
     data <- metadata_raw
-    
+
     if (input$data_holding != "All") {
-      data <- data[data$var_data_holding == input$data_holding,]
+      data <- data[data$var_data_holding == input$data_holding, ]
     }
     if (input$lib != "All") {
       # use dplyr::filter, the base R approach was not working
-      data <- dplyr::filter(data, var_lib == input$lib)
+      data <- dplyr::filter(data, .data$var_lib == input$lib)
     }
     if (input$dataset != "All") {
-      data <- data[data$var_ds == input$dataset,]
+      data <- data[data$var_ds == input$dataset, ]
     }
-    
+
+    # nolint start
     # FIXME -- Commenting these out for now pending advice from Vincent
     # if (input$lang_var != "All") {
     #   data <- data[data$lang_var_confirmed_VMS == input$lang_var,]
@@ -114,39 +148,49 @@ server <- function(input, output, session) {
     # if (input$lang_off != "All") {
     #   data <- data[data$official_lang_var_VMS == input$lang_off,]
     # }
-    
+    # nolint end
+
     data
   }) # end reactive_filtered_data()
-  
-  
+
+
   # Filter data based on selections
-  output$table <- DT::renderDataTable(DT::datatable({
-    reactive_filtered_data()
-  }, 
-  options = list(
-    pageLength = 10,      # Set number of rows per page
-    autoWidth = TRUE,     # Adjust column width automatically
-    searchHighlight = TRUE,  # Highlight search results
-    search = list(regex = TRUE, caseInsensitive = TRUE),  # Enable regex, case-insensitive search
-    scrollX = TRUE        # Enable horizontal scrolling
-  ),
-  filter = 'top' ))
-  
-  
-  
+  output$table <- DT::renderDataTable(DT::datatable(
+    {
+      reactive_filtered_data()
+    },
+    options = list(
+      pageLength = 10, # Set number of rows per page
+      autoWidth = TRUE, # Adjust column width automatically
+      searchHighlight = TRUE, # Highlight search results
+      # Enable regex, case-insensitive search
+      search = list(regex = TRUE, caseInsensitive = TRUE),
+      scrollX = TRUE # Enable horizontal scrolling
+    ),
+    filter = "top"
+  ))
+
+
+
   output$downloadData <- downloadHandler(
     filename = function() {
-      paste('data-', Sys.Date(), '.csv', sep='')
+      paste("data-", Sys.Date(), ".csv", sep = "")
     },
     content = function(con) {
       write.csv(reactive_filtered_data(), con)
     }
   )
-  
 }
 
 
 
 
-# Run the application 
-shinyApp(ui = ui, server = server, options = list(port=8080, host='0.0.0.0'))
+# Run the application
+shinyApp(
+  ui = ui,
+  server = server,
+  options = list(
+    port = 8080,
+    host = "0.0.0.0"
+  )
+)
--- utils/trim_data.R
+++ utils/trim_data.R
@@ -3,41 +3,61 @@
 
 library(dplyr)
 library(readr)
+set.seed(1)
 
-variables <- c("var_name",	"var_descr",	"Type",	"var_data_holding",	"var_ds"	,
-               "var_links",	"var_lib",	"var_url", "Population", "DCE", 
-               "official_lang_var_VMS", "official_lang_var_CP")
-
-
-  metadata_import <- readr::read_delim("var_all_consolidated.csv", 
-                                       delim = "|", escape_double = FALSE, trim_ws = TRUE)
-
-metadata_lang_manual_check <- readr::read_csv("data_concordance_output_20240116.csv", 
-                                              col_types = cols(`300339` = col_skip(), 
-                                                               ds_ref = col_character(), tbl_name = col_character(), 
-                                                               lang_var_confirmed_VMS = col_logical(), 
-                                                               official_lang_var_VMS = col_logical(), 
-                                                               lang_var_confirmed_CP = col_logical(), 
-                                                               official_lang_var_CP = col_logical(), 
-                                                               lang_var_concordance = col_logical(), 
-                                                               lang_off_concordance = col_logical(), 
-                                                               `0.980929394012633` = col_skip()))
+variables <- c(
+  "var_name", "var_descr", "Type", "var_data_holding", "var_ds",
+  "var_links", "var_lib", "var_url", "Population", "DCE",
+  "official_lang_var_VMS", "official_lang_var_CP"
+)
 
 
-metadata_raw <- dplyr::left_join(x = metadata_import,
-                                 y = metadata_lang_manual_check,
-                                 by = c("key",
-                                        "var_name",
-                                        "var_descr",
-                                        "var_data_holding",
-                                        "var_ds",
-                                        "var_lib",
-                                        "ds_ref",
-                                        "tbl_name"
-                                 )
+metadata_import <- readr::read_delim("../data/var_all_consolidated.csv",
+  delim = "|", escape_double = FALSE, trim_ws = TRUE
+)
+
+metadata_lang_manual_check <- readr::read_csv(
+  "../data/data_concordance_output_20240116.csv",
+  col_types = cols(
+    `300339` = col_skip(),
+    ds_ref = col_character(), tbl_name = col_character(),
+    lang_var_confirmed_VMS = col_logical(),
+    official_lang_var_VMS = col_logical(),
+    lang_var_confirmed_CP = col_logical(),
+    official_lang_var_CP = col_logical(),
+    lang_var_concordance = col_logical(),
+    lang_off_concordance = col_logical(),
+    `0.980929394012633` = col_skip()
+  )
+)
+
+
+metadata_raw <- dplyr::left_join(
+  x = metadata_import,
+  y = metadata_lang_manual_check,
+  by = c(
+    "key",
+    "var_name",
+    "var_descr",
+    "var_data_holding",
+    "var_ds",
+    "var_lib",
+    "ds_ref",
+    "tbl_name"
+  )
 ) |>
   dplyr::select(dplyr::all_of(variables))
 
 object.size(metadata_raw)
 
-readr::write_csv(metadata_raw, paste0("var-trimmed-", Sys.Date(),".csv"))
+metadata_nomalestrom <- metadata_raw |>
+  dplyr::filter(.data$var_data_holding != "MAELSTROM") |>
+  dplyr::slice_sample(n = 25000)
+
+as.numeric(object.size(metadata_nomalestrom) / object.size(metadata_raw))
+
+
+readr::write_csv(
+  metadata_nomalestrom,
+  paste0("var-trimmed-", Sys.Date(), ".csv")
+)
